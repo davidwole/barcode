b74c6ac5b4f02d2114cc022ff8537287557f9fe5
davidwole <davidwole20@gmail.com>
2024-08-25 07:36:38 +0100

    Update ai.js
    Changed the prompt to return an object

--- ai.js
+++ ai.js
@@ -8,12 +8,12 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 async function checkProductRating(product) {
   // const prompt = `How would you rate the "${product}" on a scale of 1-100 from reviews around the internet, return only the score`;
-  const prompt = `What is this "${product}" product from this barcode?`;
+  const prompt = `what product is this ${product}? And what would you rate it out of 100 based on internet reviews, Please return it in an object {name: productName, score: productScore} and no other text`;
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const rating = await response.text();
 
-    return rating.trim();
+    return rating;
 
 }
 
