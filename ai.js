b669086281461584ea8714051ab525128d9ff9c0
davidwole <davidwole20@gmail.com>
2024-08-25 11:13:22 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -7,8 +7,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 
 async function checkProductRating(product) {
-  // const prompt = `How would you rate the "${product}" on a scale of 1-100 from reviews around the internet, return only the score`;
-  const prompt = `what product is this ${product}? And what would you rate it out of 100 based on internet reviews, Please return it in an object {name: productName, score: productScore} and no other text`;
+  const prompt = `what product is this ${product}? And what would you rate it out of 100 based on internet reviews, Please just return score and no other text`;
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const rating = await response.text();
