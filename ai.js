c529ae1717cd41128bea04809f69a4c1c319fb96
davidwole <davidwole20@gmail.com>
2024-08-25 07:47:21 +0100

    Update ai.js
    Removed the logs

--- ai.js
+++ ai.js
@@ -12,9 +12,8 @@ async function checkProductRating(product) {
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const rating = await response.text();
-    console.log(prompt)
-    console.log(response)
-    return JSON.parse(rating);
+    
+    return rating;
 
 }
 
