b49e7fc2cdc3f340e24e4d7be9af50fa3a7efa30
davidwole <davidwole20@gmail.com>
2024-08-25 07:44:55 +0100

    Update ai.js
    Logging some errors

--- ai.js
+++ ai.js
@@ -12,8 +12,9 @@ async function checkProductRating(product) {
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const rating = await response.text();
-
-    return rating;
+    console.log(prompt)
+    console.log(response)
+    return JSON.parse(rating);
 
 }
 
