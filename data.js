8d8e3e70236a77f478ebaf623a11627b3e4bd5f3
davidwole <davidwole20@gmail.com>
2024-08-26 04:27:38 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,17 @@
 const data = 
 [
-    {
-        title: ,
-        image: ,
-        barcode: ,
-        rating: 
-    }
+ const dogFoodRatings = [
+  { title: "4Health Puppy Formula Dog Food Dry", rating: 5 },
+  { title: "4Health Small Bites Formula Dog Food Dry", rating: 5 },
+  { title: "4Health Large Breed Formula Dog Food Dry", rating: 4.5 },
+  { title: "4Health Chicken and Rice Formula Dog Food Dry", rating: 5 },
+  { title: "4Health Salmon and Potato Formula Dog Food Dry", rating: 4.5 },
+  { title: "4Health Performance Formula Dog Food Dry", rating: 5 },
+  { title: "4Health Mature Adult Formula Dog Food Dry", rating: 3 },
+  { title: "4Health Lamb and Rice Formula Dog Food Dry", rating: 3.5 },
+  { title: "4Health Healthy Weight Formula Dog Food Dry", rating: 3 },
+  { title: "4Health Beef and Rice Formula Dog Food Dry", rating: 4.5 },
+  { title: "4Health Large Breed Puppy Dog Food Dry", rating: 5 }
+];
+```⬤
 ];
